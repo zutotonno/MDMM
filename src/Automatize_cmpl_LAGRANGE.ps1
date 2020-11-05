70c91f2d266d0be97c70b9cfd6ac981f918e1a48
Rubenman16 <49437202+Rubenman16@users.noreply.github.com>
2020-11-05 18:52:16 +0100

    lagrange di merda

--- src/Automatize_cmpl_LAGRANGE.ps1
+++ src/Automatize_cmpl_LAGRANGE.ps1
@@ -2,7 +2,7 @@
     
 $style = [Globalization.NumberStyles]::Float
 
-$dir = dir D:\Lavoro\GitHub\MDMM\dati\datibuoni\dati1New
+$dir = dir D:\Lavoro\GitHub\MDMM\dati\datibuoni\dati1NewLAGRANGE
 foreach($d in $dir)
 {
 	$path = Join-Path -Path $d.FullName -ChildPath "*"
