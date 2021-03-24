2f94b902380973b554f7940fc624601f1998fc0d
Rubenman16 <49437202+Rubenman16@users.noreply.github.com>
2021-03-24 14:58:42 +0100

    csv colgen gap0

--- src/Automatize_cmpl.ps1
+++ src/Automatize_cmpl.ps1
@@ -2,7 +2,7 @@
     
 $style = [Globalization.NumberStyles]::Float
 
-$dir = dir D:\Lavoro\GitHub\MDMM\dati\datibuoni\dati1NewColGen
+$dir = dir D:\Lavoro\GitHub\MDMM\dati\datibuoni\dati1NewColGenGap0
 
 foreach($d in $dir)
 {
