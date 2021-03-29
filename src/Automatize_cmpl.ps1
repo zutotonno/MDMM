284de7a9d940fd3e3805d3016d09b8c078e5f928
Rubenman16 <49437202+Rubenman16@users.noreply.github.com>
2021-03-29 23:22:37 +0200

    ancora csv

--- src/Automatize_cmpl.ps1
+++ src/Automatize_cmpl.ps1
@@ -2,7 +2,7 @@
     
 $style = [Globalization.NumberStyles]::Float
 
-$dir = dir D:\Lavoro\GitHub\MDMM\dati\datibuoni\dati1NewColGenBoth
+$dir = dir D:\Lavoro\GitHub\MDMM\dati\datibuoni\dati1NewColGen25Ferrovie
 foreach($d in $dir)
 {
 	$path = Join-Path -Path $d.FullName -ChildPath "*"
